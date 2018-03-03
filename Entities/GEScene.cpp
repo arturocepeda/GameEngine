29cf8001cb6c0e076dd311c0b9a14f99287cb393
Arturo Cepeda <arturocepedaperez@gmail.com>
2018-03-03 13:15:52 +0100

    Renderables must not be queued for rendering in worker threads when using OpenGL

--- Entities/GEScene.cpp
+++ Entities/GEScene.cpp
@@ -758,7 +758,7 @@ void Scene::queueForRendering()
       if(!cRenderable->getVisible())
          continue;
 
-#if defined (GE_SCENE_JOBIFIED_UPDATE)
+#if defined (GE_SCENE_JOBIFIED_UPDATE) && !defined (GE_RENDERING_API_OPENGL)
       JobDesc sJobDesc("queueForRendering");
       sJobDesc.Task = [cRenderable, i] { RenderSystem::getInstance()->queueForRendering(cRenderable, i); };
       TaskManager::getInstance()->queueJob(sJobDesc, JobType::Frame);
