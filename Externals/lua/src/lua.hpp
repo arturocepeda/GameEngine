cef59e8a2d2bcbd32beea220f0b8895793ac4c15
Arturo Cepeda <arturocepedaperez@gmail.com>
2019-12-06 13:07:35 +0100

    Fixed Lua-related linked errors in iOS

--- Externals/lua/src/lua.hpp
+++ Externals/lua/src/lua.hpp
@@ -2,8 +2,21 @@
 // Lua header files for C++
 // <<extern "C">> not supplied automatically because Lua also compiles as C++
 
-extern "C" {
+#if !defined (GE_PLATFORM_IOS) && defined (__APPLE__)
+# if defined (TARGET_IPHONE_SIMULATOR) || defined (TARGET_OS_IPHONE)
+#  define GE_PLATFORM_IOS
+# endif
+#endif
+
+#if !defined (GE_PLATFORM_IOS)
+extern "C"
+{
+#endif
+
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
+
+#if !defined (GE_PLATFORM_IOS)
 }
+#endif
