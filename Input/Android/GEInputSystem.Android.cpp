c5cff760e2042d2edee95dcbb194389572e0c714
Arturo Cepeda <arturocepedaperez@gmail.com>
2024-10-15 01:11:31 +0200

    [Android] Added input system

---
+++ Input/Android/GEInputSystem.Android.cpp
@@ -0,0 +1,32 @@
+
+//////////////////////////////////////////////////////////////////
+//
+//  Arturo Cepeda Pérez
+//  Game Engine
+//
+//  Input system (Android)
+//
+//  --- GEInputSystem.Android.cpp ---
+//
+//////////////////////////////////////////////////////////////////
+
+#include "../GEInputSystem.h"
+
+namespace GE { namespace Input
+{
+   void InputSystem::platformInit()
+   {
+   }
+
+   void InputSystem::platformUpdate()
+   {
+   }
+
+   void InputSystem::platformShutdown()
+   {
+   }
+
+   void InputSystem::platformSetGamepadVibration(int pID, Gamepad::VibrationDevice pDevice, float pLevel)
+   {
+   }
+}}
